c635e0e130684b07e14e64c47aebb0c946e35424
devinacker <d@revenant1.net>
2018-04-07 19:41:46 -0400

    comments

--- scripts/dumptest.py
+++ scripts/dumptest.py
@@ -15,12 +15,12 @@ if title == "":
 # cart writing example
 c.write_cart(0x2220, b"\x00\x01\x02\x03")
 
-#data = c.read_banks(0xc0, 0xff, 0x0000, 0xffff)
+# dump $8000-ffff in banks $00-07
 data = c.read_banks(0x00, 0x07, 0x8000, 0xffff)
 with open("%s.sfc" % title, 'wb') as f:
 	data.tofile(f)
 	print("wrote %s.sfc successfully" % title)
-#data = c.read_banks(0x40, 0x40, 0x0000, 0x1fff)
+# dump $0000-7fff in banks $70-73
 data = c.read_banks(0x70, 0x73, 0x0000, 0x7fff)
 with open("%s.srm" % title, 'wb') as f:
 	data.tofile(f)
